e33eb54f14b2a232f18d2710c7dbc7350c9566e3
JasonKim0122 <JasonKim0122@gmail.com>
2022-05-15 15:51:39 -0500

    added HTML routes and app.listen for server

--- Develop/server.js
+++ Develop/server.js
@@ -12,3 +12,41 @@ app.use(express.urlencoded({ extended: true}));
 //Parse incoming JSON data
 app.use(express.json());
 app.use(express.static('public'));
+
+
+const savedNotes = require('./db/db.json');
+
+
+//HTML routes
+app.get('/api/notes', (req, res) => {
+    res.json(savedNotes.slice(1));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//app listen
+app.listen(PORT, () => {
+    console.log(`API server is live on port ${PORT}`);
+});
